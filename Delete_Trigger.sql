bd4f03bb3af19c62896be028bbe304d59d474dd0
MartinKemppi <120181210+MartinKemppi@users.noreply.github.com>
2023-09-04 11:04:35 +0300

    Update Delete_Trigger.sql

--- Delete_Trigger.sql
+++ Delete_Trigger.sql
@@ -13,3 +13,20 @@ delete from guest
 where id=2;
 select * from guest;
 select * from logi;
+
+
+create trigger roomKustutamine
+on room
+for delete
+as
+insert into logi(kuupaev, kasutaja, andmed, tegevus)
+select GETDATE(), USER, 
+CONCAT(deleted.number, ', ', deleted.name, ', ', deleted.status, ', ', deleted.smoke),
+'room on kustutatud'
+from deleted
+--kontroll
+
+delete from room
+where id=1;
+select * from room;
+select * from logi;
