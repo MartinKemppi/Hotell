5d1aa24fe599d8dc40a27d3f4bf41300ecd58733
MartinKemppi <120181210+MartinKemppi@users.noreply.github.com>
2023-09-04 11:03:15 +0300

    Update Update_trigger.sql

--- Update_trigger.sql
+++ Update_trigger.sql
@@ -17,3 +17,24 @@ update guest SET first_name = 'Luca'
 where id = 3;
 select * from guest;
 select * from logi;
+
+
+create trigger roomUuendamine
+on room
+for insert
+as
+insert into logi(kuupaev, kasutaja, andmed, tegevus)
+select GETDATE(), USER, 
+CONCAT('VANAD - ', deleted.number, ', ', deleted.name, ', ', deleted.status, ', ', deleted.smoke,
+' UUED - ', inserted.number, ', ', inserted.name, ', ', inserted.status, ', ', inserted.smoke),
+'guest on uuendatud'
+from deleted INNER JOIN inserted
+on  deleted.id = inserted.id
+--kontroll
+
+
+select * from room
+update room SET number = 'A101'
+where id = 1;
+select * from room;
+select * from logi;
