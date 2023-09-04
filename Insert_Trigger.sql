cc18d865970de22a8c69496fb3eb885178ce974f
MartinKemppi <120181210+MartinKemppi@users.noreply.github.com>
2023-09-04 10:59:33 +0300

    Update Insert_Trigger.sql

--- Insert_Trigger.sql
+++ Insert_Trigger.sql
@@ -13,3 +13,20 @@ insert into guest(first_name,last_name,member_since)
 values('Martin','Kemppi','2019-11-11');
 select * from guest;
 select * from logi;
+
+
+create trigger roomLisamine
+on room
+for insert
+as
+insert into logi(kuupaev, kasutaja, andmed, tegevus)
+select GETDATE(), USER, 
+CONCAT(inserted.number, ', ', inserted.name, ', ', inserted.status,  ', ', inserted.smoke),
+'room on lisatud'
+from inserted
+--kontroll
+
+insert into room(number,name,status,smoke)
+values('A100','PREMIUM','NOK',1)
+select * from room;
+select * from logi;
