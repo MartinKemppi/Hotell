ef78b62b1c49be0d7a16cc85bf7e925025ec904c
MartinKemppi <120181210+MartinKemppi@users.noreply.github.com>
2023-09-11 11:17:59 +0300

    Update Trigerid_kaks_tabelid_andmed.sql

--- Trigerid_kaks_tabelid_andmed.sql
+++ Trigerid_kaks_tabelid_andmed.sql
@@ -1,4 +1,4 @@
----trigger INSERT
+---trigger INSERT SQLS
 create trigger roomLisamine
 on room
 for insert
@@ -16,7 +16,7 @@ select * from room_type;
 select * from room;
 select * from logi;
 
----trigger UPDATE
+---trigger UPDATE SQLS
 create trigger roomUuendamine
 on room
 for update
@@ -38,3 +38,27 @@ update room set room_type_id = 2
 where name = 'VIP';
 select * from room;
 select * from logi;
+
+---trigger INSERT XAMPP
+insert into logi(kuupaev, andmed, kasutaja)
+select NOW(),
+CONCAT('uued andmed - ',NEW.number,', ', NEW.name,', ', NEW.status,', ', NEW.smoke,', ', rt.id),
+USER()
+FROM room r
+inner join room_type rt
+on r.room_type_id = rt.id
+where r.id = NEW.id
+
+
+---trigger UPDATE XAMPP
+insert into logi(kuupaev, andmed, kasutaja)
+select GETDATE(),
+CONCAT(
+'VANAD andmed - ', deleted.number,', ', deleted.name,', ', deleted.status,', ', deleted.smoke,', ', rt1.id,
+' UUED andmed - ', inserted.number,', ', inserted.name,', ', inserted.status,', ', inserted.smoke,', ', rt2.id
+),
+USER
+FROM deleted
+inner join inserted on deleted.id = inserted.id
+inner join room_type rt1 on deleted.room_type_id = rt1.id
+inner join room_type rt2 on inserted.room_type_id = rt2.id
