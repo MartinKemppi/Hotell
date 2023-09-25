3358534ac59b2de5fafcd7802f95072124786ebd
MartinKemppi <120181210+MartinKemppi@users.noreply.github.com>
2023-09-25 11:26:35 +0300

    uuendatud view.sql

--- view.sql
+++ view.sql
@@ -1,3 +1,108 @@
+--36 klasteeritud ja mitte klasteeritud indexid
+
+create clustered index IX_tblEmployee_Name
+on DimEmployee(FirstName)
+
+select * from DimEmployee
+
+create table tblEmployee
+(
+Id int primary key,
+Name nvarchar(50),
+Salary int,
+Gender nvarchar(10),
+City nvarchar(50)
+);
+
+insert into tblEmployee values(3,'John',4500,'Male','New York');
+insert into tblEmployee values(1,'Sam',2500,'Male','London');
+insert into tblEmployee values(4,'Sara',5500,'Female','Tokyo');
+insert into tblEmployee values(5,'Todd',3100,'Male','Toronto');
+insert into tblEmployee values(2,'Pam',6500,'Female','Sydney');
+
+drop index tblEmployee.PK__tblEmplo_3214EC07180FD880
+
+create clustered index IX_tblEmployee_Gender_Salary
+on tblEmployee(Gender DESC, Salary ASC)
+
+select * from tblEmployee
+
+create nonclustered index IX_tblEmployee_Name
+on tblEmployee(Name)
+
+select * from tblEmployee
+
+drop table tblEmployee
+
+--37 unikaalne ja mitte unikaalne index
+
+
+create table tblEmployee
+(
+Id int primary key,
+FirstName nvarchar(50),
+LastName nvarchar(50),
+Salary int,
+Gender nvarchar(10),
+City nvarchar(50)
+);
+
+execute sp_helpindex tblEmployee
+
+insert into tblEmployee values(1,'Mike','Sandoz',4500,'Male','New York');
+insert into tblEmployee values(1,'John','Menco',2500,'Male','London');
+
+select * from tblEmployee
+
+drop index PK__tblEmplo_3214EC0780489261
+
+Create Unique NonClustered Index UIX_tblEmployee_FirstName_LastName
+On tblEmployee(FirstName, LastName)
+
+
+delete from tblEmployee
+where id = 1
+
+alter table tblEmployee
+add constraint UQ_tblEmployee_City
+unique nonclustered (City)
+
+execute sp_helpconstraint tblEmployee
+
+create unique index IX_tblEmployee_City
+on tblEmployee(City)
+with ignore_dup_key
+
+--38 index plussid ja miinused 
+
+delete from tblEmployee
+where id = 1
+
+insert into tblEmployee values(1,'Mike','Sandoz',4500,'Male','New York');
+insert into tblEmployee values(2,'Sara','Menco',6500,'Female','London');
+insert into tblEmployee values(3,'John','Barber',2500,'Male','Sydney');
+insert into tblEmployee values(4,'Pam','Grove',3500,'Female','Toronto');
+insert into tblEmployee values(5,'James','Mirch',7500,'Male','London');
+
+create nonclustered index IX_tblEmployee_Salary
+on tblEmployee (Salary ASC)
+
+select * from tblEmployee
+
+select * from tblEmployee where Salary > 4000 and Salary < 8000
+
+delete from tblEmployee where Salary = 2500;
+
+update tblEmployee set Salary = 9000 where Salary = 7500;
+
+select * from tblEmployee order by Salary;
+
+select * from tblEmployee order by Salary DESC;
+
+select Salary, COUNT(Salary) as Total
+from tblEmployee
+group by Salary;
+
 --39 View SQL serveris
 
 drop table tblEmployee
@@ -62,6 +167,18 @@ on tblEmployee.DepartmentId = tblDepartment.DeptId;
 
 select * from vWEmployeesNonConfidentialData
 
+create view vWEmployeesCountByDepartment
+as
+select DeptName, Count(Id) as TotalEmployees
+from tblEmployee
+join tblDepartment
+on tblEmployee.DepartmentId = tblDepartment.DeptId
+group by DeptName
+
+select * from vWEmployeesCountByDepartment
+
+execute sp_helptext vWName
+
 --40 View uuendused
 
 create view vWEmployeesDataExceptSalary
@@ -88,6 +205,9 @@ select * from vwEmployeeDetailsByDepartment
 
 update vwEmployeeDetailsByDepartment
 set DeptName = 'IT' Where Name = 'John'
+
+select * from tblEmployee;
+select * from tblDepartment;
 
 --41 indekseeritud view
 
