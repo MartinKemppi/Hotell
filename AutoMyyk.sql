98b04c9a5d51c950cbcc15dfa70e6b4dc5ce5bac
MartinKemppi <120181210+MartinKemppi@users.noreply.github.com>
2023-10-18 13:37:55 +0300

    Add files via upload

--- AutoMyyk.sql
+++ AutoMyyk.sql
@@ -1,5 +1,5 @@
-create table Teenus(
-teenus_ID int primary key identity(1,1),
+create table Teenuse_tyyp(
+teenuse_tyyp_ID int primary key identity(1,1),
 ost varchar(3),
 hooldamine varchar(3),
 lising varchar(3),
@@ -30,8 +30,15 @@ create table Klient(
 klient_ID int primary key identity(1,1),
 nimi varchar(10),
 perenimi varchar(35),
-teenus int foreign key references Teenus(teenus_ID),
-salong int foreign key references Salong(salong_ID)
+telefon varchar(8)
+);
+
+create table Teenus(
+teenus_id int primary key identity(1,1),
+klient int foreign key references Klient(klient_id),
+teenuse_tyyp int foreign key references Teenuse_tyyp(teenuse_tyyp_id),
+salong int foreign key references Salong(salong_id),
+tootaja int foreign key references Tootaja(tootaja_id)
 );
 
 create table Keretyyp(
@@ -87,25 +94,40 @@ kaigukast int foreign key references Kaigukast(kaigukast_ID),
 salong int foreign key references Salong(salong_ID)
 );
 
-insert into Teenus(ost,hooldamine,lising,test_soit)
+select * from Aasta
+select * from Auto
+select * from Hind
+select * from Kaigukast
+select * from Keretyyp
+select * from Klient
+select * from Koht
+select * from kW
+select * from Kytus
+select * from Mark
+select * from Mudel
+select * from Salong
+select * from Teenus
+select * from Tootaja
+
+insert into Teenuse_tyyp(ost,hooldamine,lising,test_soit)
 values('ei','ei','ei','ei');
-insert into Teenus(ost,hooldamine,lising,test_soit)
+insert into Teenus_tyyp(ost,hooldamine,lising,test_soit)
 values('jah','jah','jah','jah');
-insert into Teenus(ost,hooldamine,lising,test_soit)
+insert into Teenus_tyyp(ost,hooldamine,lising,test_soit)
 values('ei','ei','ei','jah');
-insert into Teenus(ost,hooldamine,lising,test_soit)
+insert into Teenus_tyyp(ost,hooldamine,lising,test_soit)
 values('ei','jah','ei','ei');
-insert into Teenus(ost,hooldamine,lising,test_soit)
+insert into Teenus_tyyp(ost,hooldamine,lising,test_soit)
 values('jah','ei','ei','ei');
-insert into Teenus(ost,hooldamine,lising,test_soit)
+insert into Teenus_tyyp(ost,hooldamine,lising,test_soit)
 values('jah','ei','jah','ei');
-insert into Teenus(ost,hooldamine,lising,test_soit)
+insert into Teenus_tyyp(ost,hooldamine,lising,test_soit)
 values('jah','jah','jah','ei');
-insert into Teenus(ost,hooldamine,lising,test_soit)
+insert into Teenus_tyyp(ost,hooldamine,lising,test_soit)
 values('jah','jah','ei','ei');
-insert into Teenus(ost,hooldamine,lising,test_soit)
+insert into Teenus_tyyp(ost,hooldamine,lising,test_soit)
 values('jah','jah','ei','jah');
-insert into Teenus(ost,hooldamine,lising,test_soit)
+insert into Teenus_tyyp(ost,hooldamine,lising,test_soit)
 values('ei','jah','ei','jah');
 
 insert into Koht(linn, aadress)
@@ -149,6 +171,7 @@ insert into Salong(nimi, koht)
 values('Allcars',9);
 insert into Salong(nimi, koht)
 values('SCC',10);
+
 
 insert into Tootaja(nimi, perenimi, amet, salong)
 values('Martin','Kemppi','Esimees',1);
@@ -260,41 +283,41 @@ values('Bill','Haven','Administrator',10);
 insert into Tootaja(nimi, perenimi, amet, salong)
 values('Jukko','Moisio','Tehnik',10);
 
-insert into Klient(nimi, perenimi, teenus, salong)
-values('Andre','Giant',2,1);
-insert into Klient(nimi, perenimi, teenus, salong)
-values('Barack','Obama',2,1);
-insert into Klient(nimi, perenimi, teenus, salong)
-values('Dwayne','Johnson',1,1);
-insert into Klient(nimi, perenimi, teenus, salong)
-values('Donald','Trump',1,1);
-insert into Klient(nimi, perenimi, teenus, salong)
-values('Jericho','Iniko',3,3);
-insert into Klient(nimi, perenimi, teenus, salong)
-values('Aleksandr','Bellen',3,3);
-insert into Klient(nimi, perenimi, teenus, salong)
-values('Georg','Ots',4,4);
-insert into Klient(nimi, perenimi, teenus, salong)
-values('Katalin','Novak',4,4);
-insert into Klient(nimi, perenimi, teenus, salong)
-values('Freidrich','Kreutswald',5,5);
-insert into Klient(nimi, perenimi, teenus, salong)
-values('Frank','SteinMeier',5,5);
-insert into Klient(nimi, perenimi, teenus, salong)
-values('Anton','Tammsaare',6,6);
-insert into Klient(nimi, perenimi, teenus, salong)
-values('Michael','Higgins',6,6);
-insert into Klient(nimi, perenimi, teenus, salong)
-values('Michael','Jackson',7,7);
-insert into Klient(nimi, perenimi, teenus, salong)
-values('Serjo','Matarella',7,7);
-insert into Klient(nimi, perenimi, teenus, salong)
-values('Tom','Hardy',8,8);
-insert into Klient(nimi, perenimi, teenus, salong)
-values('Andjey','Duda',8,8);
-insert into Klient(nimi, perenimi, teenus, salong)
-values('Woody','Harrelson',9,9);
-insert into Klient(nimi, perenimi, teenus, salong)
-values('Sauli','Niinistö',9,9);
-insert into Klient(nimi, perenimi, teenus, salong)
-values('Alan','Karris',10,10);
+insert into Klient(nimi, perenimi, telefon)
+values('Andre','Giant','50034348');
+insert into Klient(nimi, perenimi, telefon)
+values('Barack','Obama','52738263');
+insert into Klient(nimi, perenimi, telefon)
+values('Dwayne','Johnson','57635987');
+insert into Klient(nimi, perenimi, telefon)
+values('Donald','Trump','58132610');
+insert into Klient(nimi, perenimi, telefon)
+values('Jericho','Iniko','50509113');
+insert into Klient(nimi, perenimi, telefon)
+values('Aleksandr','Bellen','51583759');
+insert into Klient(nimi, perenimi, telefon)
+values('Georg','Ots','57980491');
+insert into Klient(nimi, perenimi, telefon)
+values('Katalin','Novak','52400489');
+insert into Klient(nimi, perenimi, telefon)
+values('Freidrich','Kreutswald','50641509');
+insert into Klient(nimi, perenimi, telefon)
+values('Frank','SteinMeier','52701481');
+insert into Klient(nimi, perenimi, telefon)
+values('Anton','Tammsaare','57196846');
+insert into Klient(nimi, perenimi, telefon)
+values('Michael','Higgins','53167728');
+insert into Klient(nimi, perenimi, telefon)
+values('Michael','Jackson','57298118');
+insert into Klient(nimi, perenimi, telefon)
+values('Serjo','Matarella','52861579');
+insert into Klient(nimi, perenimi, telefon)
+values('Tom','Hardy','58641506');
+insert into Klient(nimi, perenimi, telefon)
+values('Andjey','Duda','54268224');
+insert into Klient(nimi, perenimi, telefon)
+values('Woody','Harrelson','52693115');
+insert into Klient(nimi, perenimi, telefon)
+values('Sauli','Niinistö','55012628');
+insert into Klient(nimi, perenimi, telefon)
+values('Alan','Karris','55335568');
